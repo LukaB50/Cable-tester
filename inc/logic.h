c579167ef942da2c144e2d5c8b8403376e87af03
LukaB50 <lb49283@fer.hr>
2018-09-06 16:34:02 +0200

    funkcije prebacene u logic.c i logic.h

---
+++ inc/logic.h
@@ -0,0 +1,10 @@
+#ifndef __LOGIC_H
+#define __LOGIC_H
+
+void posalji_matricu(uint8_t* p);
+int dodaj_novi(void);
+void saznaj_ocitani(void);
+void primi_ispravan(void);
+int provjeri_pinout(void);
+
+#endif
